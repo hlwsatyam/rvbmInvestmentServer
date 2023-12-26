3367765e495d75be3d3aebcae0cbca887fdca034
satyam kumar <satyampandit021@gmail.com>
2023-12-26 12:59:02 +0530

    update:spare and index added

--- Model/clientDetail.js
+++ Model/clientDetail.js
@@ -1,3 +1,75 @@
+// const mongoose = require("mongoose");
+
+// const clientSchema = new mongoose.Schema(
+//   {
+//     full_name: {
+//       type: String,
+//     },
+//     step: {
+//       type: Number,
+//       default: 0,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     phone_number: {
+//       type: String,
+//       unique: true,
+//       required: true,
+//     },
+//     postal: {
+//       type: Number,
+//       required: true,
+//     },
+//     current_address: {
+//       type: String,
+//     },
+//     permanent_address: {
+//       type: String,
+//     },
+
+//     is_KYC_completed: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     gender: {
+//       type: String,
+//     },
+//     fd_number: {
+//       type: String,
+//       default: null,
+//     },
+//     aadhaar: {
+//       type: Number,
+//       unique: true,
+//     },
+
+//     pan_number: {
+//       type: String,
+//       unique: true,
+//     },
+//     aadhaar_details: {
+//       type: mongoose.Schema.Types.Mixed,
+//     },
+//     banks_details: {
+//       type: mongoose.Types.ObjectId,
+//       ref: "clientbank",
+//     },
+//     finance_details: [
+//       { type: mongoose.Schema.Types.ObjectId, ref: "clientfinance" },
+//     ],
+//   },
+//   { timestamps: true }
+// );
+
+// const ClientModel = mongoose.model("client", clientSchema);
+
+// module.exports = { ClientModel };
+
+
+
 const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema(
@@ -12,11 +84,16 @@ const clientSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
-      unique: true,
+      index:true,
+      unique:true,
+      sparse:true
+
     },
     phone_number: {
       type: String,
-      unique: true,
+      index:true,
+      unique:true,
+      sparse:true
       required: true,
     },
     postal: {
@@ -39,16 +116,23 @@ const clientSchema = new mongoose.Schema(
     },
     fd_number: {
       type: String,
-      default: null,
+      index:true,
+      unique:true,
+      sparse:true
     },
     aadhaar: {
       type: Number,
-      unique: true,
+     
+      index:true,
+      unique:true,
+      sparse:true
     },
 
     pan_number: {
       type: String,
-      unique: true,
+      index:true,
+      unique:true,
+      sparse:true
     },
     aadhaar_details: {
       type: mongoose.Schema.Types.Mixed,
